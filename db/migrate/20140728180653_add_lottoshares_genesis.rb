8bb12cded17c1c57469d54c31e1a48fa3ed68b64
Alex Chien <alex.chien@koocaa.com>
2014-08-01 16:41:43 +0800

    update migration

--- db/migrate/20140728180653_add_lottoshares_genesis.rb
+++ db/migrate/20140728180653_add_lottoshares_genesis.rb
@@ -1,36 +1,51 @@
 # -*- encoding : utf-8 -*-
 class AddLottosharesGenesis < ActiveRecord::Migration
+  def import_data(addresses)
+    addresses.each { |data| data.push('LTS') }
+
+    DacGenesis.import [:address, :amount, :dac], addresses, validate: false
+  end
+
   def up
-    addresses = []
     # angelshares
+    addresses = []
     File.read("#{Rails.root}/db/migrate/lottoshares/angelshares.txt").each_line do |line|
       address, amount = line.gsub("\n","").split(':')
       addresses.push([address, amount])
     end
+    import_data(addresses)
 
     # bitcoin
+    addresses = []
     File.read("#{Rails.root}/db/migrate/lottoshares/bitcoin.txt").each_line do |line|
       address = line.gsub("\n","")
       addresses.push([address, 2_000_000_000])
     end
+    import_data(addresses)
 
     # protoshares
+    addresses = []
     File.read("#{Rails.root}/db/migrate/lottoshares/protoshares.txt").each_line do |line|
       address, amount = line.gsub("\n","").split(':')
       addresses.push([address, amount])
     end
+    import_data(addresses)
 
     # thirty percent
+    addresses = []
     File.read("#{Rails.root}/db/migrate/lottoshares/thirtypercent.txt").each_line do |line|
       nickname, address, amount = line.gsub("\n","").split(',')
       addresses.push([address, amount])
     end
+    import_data(addresses)
 
     # thirty percent 2
+    addresses = []
     File.read("#{Rails.root}/db/migrate/lottoshares/thirtypercent2.txt").each_line do |line|
       nickname, address, amount = line.gsub("\n","").split(',')
       addresses.push([address, amount])
     end
+    import_data(addresses)
 
     # dogecoin
     # skip
@@ -47,13 +62,10 @@ class AddLottosharesGenesis < ActiveRecord::Migration
     #   DacGenesis.create(dac: 'LTS', address: data[0], amount: data[1].to_i)
     # end
 
-    addresses.each { |data| data.push('LTS') }
-
-    DacGenesis.import [:address, :amount, :dac], addresses, validate: false
 
   end
 
   def down
-    DacGenesis.where(dac: 'LTC').delete_all
+    DacGenesis.where(dac: 'LTS').delete_all
   end
 end
